e9b752f2a7e3a40beb5ceeb65292e41c649580ee
dog_ears <info@dog-ears.net>
2016-03-22 07:45:01 +0000

    bug fix

--- src/GeneratorsServiceProvider.php
+++ src/GeneratorsServiceProvider.php
@@ -51,25 +51,25 @@ class GeneratorsServiceProvider extends ServiceProvider {
 	{
 		//Create scaffold
 		$this->app->singleton('command.CrudDscaffold.scaffold', function ($app) {
-			return $app['dogears\crud-d-scaffold\Commands\ScaffoldMakeCommand'];
+			return $app['dogears\CrudDscaffold\Commands\ScaffoldMakeCommand'];
 		});
 		$this->commands('command.CrudDscaffold.scaffold');
 
 		//Delete scaffold
 		$this->app->singleton('command.CrudDscaffold.scaffold_delete', function ($app) {
-			return $app['dogears\crud-d-scaffold\Commands\ScaffoldDeleteCommand'];
+			return $app['dogears\CrudDscaffold\Commands\ScaffoldDeleteCommand'];
 		});
 		$this->commands('command.CrudDscaffold.scaffold_delete');
 
 		//make:relation
 		$this->app->singleton('command.CrudDscaffold.make_relation', function ($app) {
-			return $app['dogears\crud-d-scaffold\Commands\MakeRelationCommand'];
+			return $app['dogears\CrudDscaffold\Commands\MakeRelationCommand'];
 		});
 		$this->commands('command.CrudDscaffold.make_relation');
 
 		//delete:relation
 		$this->app->singleton('command.CrudDscaffold.delete_relation', function ($app) {
-			return $app['dogears\crud-d-scaffold\Commands\DeleteRelationCommand'];
+			return $app['dogears\CrudDscaffold\Commands\DeleteRelationCommand'];
 		});
 		$this->commands('command.CrudDscaffold.delete_relation');
 
