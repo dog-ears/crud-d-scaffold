253451e9fd0a280d06de8aed1364ba37378a4307
dog-ears <info@dog-ears.net>
2018-09-27 15:32:16 +0900

    [bugfix] count not countable error

--- src/MyClass/Model.php
+++ src/MyClass/Model.php
@@ -32,7 +32,7 @@ class Model
         $result = current( array_filter( $this->schemas, function($schema) use($name){
             return $schema->name === $name;
         }));
-        if( count($result)===0 ){
+        if( $result===FALSE ){
             throw new \Exception('getSchemaByName('.$name.') return no schema!');
         }
         return $result;
